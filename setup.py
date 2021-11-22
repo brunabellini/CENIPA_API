0088665ac6784bcae576dd6d39fdc95bc1f4d906
Bruna Bellini Faria <80490047+brunabellini@users.noreply.github.com>
2021-11-22 15:13:17 -0300

    setup pypi

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ setup(
     name = 'CENIPA_API',
     version = '0.0.2',
     description = 'Coleta e análise dos últimos 10 anos de ocorrencias registradas pelo CENIPA',
-    long_description = 'Coleta e análise dos últimos 10 anos de ocorrencias registradas pelo CENIPA para '
-                        + 'retornar datasets individuais com informações sobre, acidentes, fatores contribuintes, aeronave e recomendações, '
-                        + 'retornar insights das informações (colunas) do dataset',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
     author = 'Alunos Ciência de Dados PUC-SP',
     packages = ['CENIPA_API'],
     license= 'MIT',
-    keywords = ['API', 'acidentes', 'CENIPA', ],
+    keywords = ['API', 'acidentes', 'CENIPA', 'analise de dados'],
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
